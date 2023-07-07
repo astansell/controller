a2f41c58ffceebb9f9aed7f68863f5de39a4e377
astansell <33355480+astansell@users.noreply.github.com>
2023-07-07 17:03:34 -0400

    Update controller.cpp

--- controller.cpp
+++ controller.cpp
@@ -14,14 +14,11 @@ namespace Kitronik_Game_Controller {
         if (initialized) return;
         //This function sets all the buttons on the :GAME Controller to actually appear as buttons on the micro:bit
         #define ALLOC_PIN_BUTTON(id) new MicroBitButton(getPin(id)->name, id, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
-            ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P0)
-            ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P1)
-            ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P2)
             ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P8)
+            ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P0)
             ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P12)
-            ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P13)
-            ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P14)
-            ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P15)
+            ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P2)            
+            ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P1)
             ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P16)
         #undef ALLOC_PIN_BUTTON
 
